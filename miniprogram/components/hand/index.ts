5c1e4d93f47c47f6e7a56277ab6975d2de1a699b
panjiajie <panjiajie@chexiao.co>
2025-05-14 17:09:53 +0800

    commit

--- miniprogram/components/hand/index.ts
+++ miniprogram/components/hand/index.ts
@@ -2,10 +2,18 @@ const common = require("../../behavior/index");
 Component({
   behaviors: [common],
   properties: {},
-  data: { scene: null },
+  data: { scene: null, loaded: false },
   methods: {
     handleReady({ detail }) {
       this.setData({ scene: detail.value });
+      this.handleAssetsLoaded();
+    },
+    handleAssetsLoaded() {
+      this.setData({ loaded: true });
+      const el = this.scene.getElementById("tracker");
+      this.tracker = el.getComponent(wx.getXrFrameSystem().ARTracker);
+      this.gesture = -1;
+      console.log(this.tracker, this.gesture);
     },
   },
 });
