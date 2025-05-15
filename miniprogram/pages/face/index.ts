e945dfa621eca6fc8aefe921c193349d161edba8
panjiajie <panjiajie@chexiao.co>
2025-05-15 11:09:32 +0800

    face

--- miniprogram/pages/face/index.ts
+++ miniprogram/pages/face/index.ts
@@ -1,66 +1,16 @@
-// pages/face/index.ts
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    renderWidth: 0,
+    renderHeight: 0,
+    model: "",
+    arSystem: "modes:Plane Marker; planeMode: 1",
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad() {
-
+    const info = wx.getWindowInfo();
+    const { screenHeight, screenWidth } = info;
+    this.setData({
+      renderWidth: screenWidth,
+      renderHeight: screenHeight,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+});
