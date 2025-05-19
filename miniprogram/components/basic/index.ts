a31ed70e4e6319a8a23263e08ffec2e7b5f07533
panjiajie <panjiajie@chexiao.co>
2025-05-19 16:06:01 +0800

    材质

--- miniprogram/components/basic/index.ts
+++ miniprogram/components/basic/index.ts
@@ -4,9 +4,23 @@ Component({
       type: Object,
       value: () => {},
     },
+    material: {
+      type: String,
+      value: "",
+    },
   },
-
-  data: {},
+  observers: {
+    material(material) {
+      material
+        ? this.setData({ materialName: `u_baseColorMap: ${material}` })
+        : this.setData({
+            materialName: "u_baseColorFactor:1 1 1",
+          });
+    },
+  },
+  data: {
+    materialName: "",
+  },
 
   methods: {},
 });
--- miniprogram/pages/basic/index.ts
+++ miniprogram/pages/basic/index.ts
@@ -4,12 +4,25 @@ Page({
     renderHeight: 0,
     config: {
       shadow: false,
+      material: false,
     },
+    material: "",
   },
   changeShadow() {
     const shadow = !this.data.config.shadow;
     this.setData({
-      config: { ...this.data.config, shadow },
+      config: { ...this.data.config, shadow, material: false },
+      material: "",
+    });
+  },
+  changeMaterial(e: { target: { dataset: { material: string } } }) {
+    const material = e.target.dataset.material;
+    this.setData({
+      config: {
+        ...this.data.config,
+        material: material ? true : !this.data.config.material,
+      },
+      material: material || "",
     });
   },
   onLoad() {
