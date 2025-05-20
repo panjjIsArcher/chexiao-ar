466d1b16d35bdf7591d0c46c2e8c044cea718c54
panjiajie <panjiajie@chexiao.co>
2025-05-20 15:03:58 +0800

    视频纹理

--- miniprogram/components/basic/index.ts
+++ miniprogram/components/basic/index.ts
@@ -11,12 +11,46 @@ Component({
   },
   observers: {
     material(material) {
-      material
-        ? this.setData({ materialName: `u_baseColorMap: ${material}` })
-        : this.setData({ materialName: "u_baseColorFactor:1 1 1" });
+      if (material === "video") {
+        this.setData({ materialName: `u_baseColorMap: ${material}` });
+        this.setData({ background: "skybox" });
+      } else if (material === "") {
+        this.setData({ materialName: "u_baseColorFactor:1 1 1" });
+        this.setData({ background: "default" });
+        this.data.isPlay = true;
+        this.play();
+      } else {
+        this.setData({ materialName: `u_baseColorMap: ${material}` });
+        this.setData({ background: "default" });
+        this.data.isPlay = true;
+        this.play();
+      }
     },
   },
   data: {
     materialName: "",
+    background: "default",
+    scene: null,
+    isPlay: false,
+  },
+  methods: {
+    handleReady({ detail }) {
+      this.data.scene = detail.value;
+    },
+    play() {
+      const xrSystem = wx.getXrFrameSystem();
+      const video = this.data.scene.assets.getAsset("video-texture", "cat");
+
+      if (!video) {
+        return;
+      }
+      const isPlay = this.data.isPlay;
+      this.setData({ isPlay: !isPlay });
+      if (isPlay) {
+        video.pause();
+      } else {
+        video.play();
+      }
+    },
   },
 });
